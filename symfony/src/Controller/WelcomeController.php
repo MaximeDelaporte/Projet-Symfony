7b86ce98cf90c0833151da7e2d4299311cd2ad1a
Quentin Andries <andries.quentin@gmail.com>
2018-06-19 12:07:33 +0200

    update

--- symfony/src/Controller/WelcomeController.php
+++ symfony/src/Controller/WelcomeController.php
@@ -4,16 +4,38 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use App\Service\MessageGenerator;
 
 class WelcomeController extends Controller
 {
     /**
+     * Matches /welcome exactly
+     *
      * @Route("/welcome", name="welcome")
      */
     public function index()
     {
-        return $this->render('welcome/index.html.twig', [
-            'user_name' => 'Me Myself and I',
+        return $this->render('welcome/index.html.twig');
+    }
+
+    /**
+     * Matches /welcome/*
+     *
+     * @Route("/welcome/login", name="welcome_login")
+     */
+
+    public function connected()
+    {
+        return $this->render('welcome/connected.html.twig', [
+            'user_name' => 'My my',
         ]);
+    }
+    /**
+     * @Route("/welcome/disconnected", name="disconnecting")
+     */
+
+    public function disconnected()
+    {
+        return $this->redirectToRoute('welcome');
     }
 }
