e04e92d32defc483563787eb3aa2f1347c0c8145
Unknown <s.gaudefroy@orange.fr>
2018-06-19 15:37:22 +0200

    Create room registration

---
+++ symfony/src/Controller/RoomRegistration.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Room;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class RommRegistration extends Controller
+{
+    /**
+     * @Route("/postRoom", name="registration")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function PostRoom(Request $request)
+    {
+        $room = new Room();
+        $form->handleRequest($request);
+        
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $room->name = $name;
+            $room->location = $location;
+            $room->city = $city;
+            $room->cp = $cp;
+            $room->description = $description;
+            $room->capacity = $capacity;
+            $room->isRented = $isRented;
+            $room->rentingDateBegin = $rentingDateBegin;
+            $room->rentingDateEnd = $rentingDateEnd;
+            
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($room);
+            $entityManager->flush();
+
+            return new Response('Saved new product with id '.$$room->id());
+            
+        }
+
+        
+    }
+}
