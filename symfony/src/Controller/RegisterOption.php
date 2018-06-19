c2ef3f348856dab12b94a485cfa496897e4bb4d4
Unknown <s.gaudefroy@orange.fr>
2018-06-19 17:03:10 +0200

    create entity rooms, registerOption and register Room

---
+++ symfony/src/Controller/RegisterOption.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Option;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RegisterOption extends Controller
+{
+    /**
+     * @Route("/postRoom", name="postRoom")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function postOption(Request $request)
+    {
+        $option = new Option();
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $option->name = $name;
+            $option->price = $price;
+                        
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($option);
+            $entityManager->flush();
+
+            return new Response('Saved new product with id '.$option->id());
+            
+        }
+    }
+}
